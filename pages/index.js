0877b21274a2df943efd9babd69b3288373a4bda
adammamaril <adammamaril@outlook.com>
2022-08-24 15:07:56 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,17 +10,17 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Hey You! Ya you! Are you seriously playing 7 Days all by yourself?" />
-		<img src="https://i.imgur.com/GeA4VOA.gif"/>
+        <Header title="4DEEZ 7DAYZ AYY LMAO EXTRAVAGANZA!!" />
+		<a href="https://www.youtube.com/watch?v=zL19uMsnpSU"> <img src="https://i.imgur.com/GeA4VOA.gif"/> </a>
         <p className="description">
-          <a href="steam://connect/50.68.198.123:26901/4deez"><h1> Join 7 Days Server Here</h1></a>
+          <a href="steam://connect/50.68.198.123:26901/4deez"><h2> Join 7 Days Server Here</h2></a>
 		<h2> Password = 4deez </h2>
-		<h2> You can also connect using 7days.4deeznuts.xyz as the IP </h2>
+		<h3> You can also connect using 7days.4deeznuts.xyz as the IP </h3>
 	  	<h3> Open Invites to DBC server and my friends so be friendly yall :) </h3>
-		<br>
-		<h4 style="color:red"> 300% XP | 160% LOOT | 150% BLOCK DMG | TWITCH CMDS </h4>
-		<br>
-		<h4 style="color:red"> SERVER MODS - SNUFKIN ZOMBS | BIGGER CRAFT QUEUE | 60 SPACE BACKPACK | ALWAYS OPEN TRADER | BUFFED SUPPLY DROPS </H4>
+		<br></br>
+		<h4> MODIFIED VALUES = 300% XP | 160% LOOT | 150% BLOCK DMG | TWITCH CMDS </h4>
+		<br></br>
+		<h4> SERVER MODS = SNUFKIN ZOMBS | BIGGER CRAFT QUEUE | 60 SPACE BACKPACK | ALWAYS OPEN TRADER | BUFFED SUPPLY DROPS </h4>
         </p>
 		
       </main>
