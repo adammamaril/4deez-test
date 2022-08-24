045b9e3f6326d4412c7aac7ef036432d1dfd905e
adammamaril <adammamaril@outlook.com>
2022-08-24 13:14:36 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <Header title="4deez Big Sexi Site" />
 		<img src="https://i.imgur.com/GeA4VOA.gif"/>
         <p className="description">
-          <a href="steam://connect/50.68.198.123:26901">Join 7 Days Server Here</a>
+          <a href="steam://connect/50.68.198.123:26901/4deez">Join 7 Days Server Here</a>
 		<h2> Password = 4deez </h2>
 		  
         </p>
